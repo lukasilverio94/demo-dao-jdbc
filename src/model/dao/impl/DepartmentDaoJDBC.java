cc04e990f71b48615bc11dc27a1a165343a68e3d
= <lukasilverio94@gmail.com>
2025-03-20 13:41:33 +0100

    fix: add finally block to insert department method

--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -47,6 +47,9 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 		catch (SQLException e) {
 			throw new DbException(e.getMessage());
 		}
+		finally {
+			DB.closeStatement(st);
+		}
 
 	}
 
