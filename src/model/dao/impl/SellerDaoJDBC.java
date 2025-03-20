96907dc3e3186964604227ecc29620ac3a58878d
= <lukasilverio94@gmail.com>
2025-03-20 11:15:45 +0100

    implements:findByDepartment returns a list of sellers that points to same object department in memory

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -4,7 +4,10 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -98,6 +101,48 @@ public class SellerDaoJDBC implements SellerDao {
 	public List<Seller> findAll() {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*,department.Name as DepName " +
+					"FROM seller INNER JOIN department " +
+					"ON seller.DepartmentId = department.Id " +
+					" WHERE DepartmentId = ? " +
+					"ORDER BY Name"
+					);
+			
+			st.setInt(1, department.getId());
+			
+			rs = st.executeQuery();
+			
+			List<Seller> sellerList = new ArrayList<>();
+			// Use Map to don't repeat department on the list
+			Map<Integer, Department> map = new HashMap<>();
+			while(rs.next()) {
+				Department dep = map.get(rs.getInt("departmentid"));
+				// check the Map if dep is null, just if is null we will instantiate it the Department				
+				if(dep == null) {
+					dep = instantiateDepartment(rs);
+					map.put(rs.getInt("departmentid"), dep);
+				}
+				
+				Seller obj = instantiateSeller(rs, dep);
+				sellerList.add(obj);					
+			}			
+			return sellerList;			
+		} catch(SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 	}
 	
 }
